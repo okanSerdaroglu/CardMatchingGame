ddfc059db3d043512839936e379a7611edf519ea
okanSerdaroglu <okanserdaroglu2@gmail.com>
2020-02-12 16:12:30 +0300

    gradle updated

---
+++ app/src/main/java/com/example/cardmatchinggame/LeadersFragment.kt
@@ -0,0 +1,15 @@
+package com.example.cardmatchinggame
+
+
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.example.cardmatchinggame.base.BaseFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class LeadersFragment : BaseFragment (){
+
+
+
+
+}
---
+++ app/src/main/java/com/example/cardmatchinggame/base/BaseFragment.kt
@@ -0,0 +1,16 @@
+package com.example.cardmatchinggame.base
+
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment (){
+
+
+}
