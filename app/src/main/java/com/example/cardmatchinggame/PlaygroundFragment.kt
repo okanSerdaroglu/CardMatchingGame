cb038f26b6f2143e9239b3a7bce6398c7701114b
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-02-13 20:33:35 +0300

    PlaygroundFragment added

---
+++ app/src/main/java/com/example/cardmatchinggame/PlaygroundFragment.kt
@@ -0,0 +1,17 @@
+package com.example.cardmatchinggame
+
+import androidx.databinding.ViewDataBinding
+import com.example.cardmatchinggame.base.BaseFragment
+import com.example.cardmatchinggame.databinding.FragmentPlaygroundBinding
+
+class PlaygroundFragment : BaseFragment<FragmentPlaygroundBinding>(){
+
+
+    override fun onBind(binding: ViewDataBinding) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getLayoutId(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
