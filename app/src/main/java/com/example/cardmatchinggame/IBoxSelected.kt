77c8c7d3507fa4fd0a392532dd13105947116658
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-02-15 01:06:00 +0300

    ImagesAdapter, ImagesViewHolder, and clickListener added

---
+++ app/src/main/java/com/example/cardmatchinggame/IBoxSelected.kt
@@ -0,0 +1,7 @@
+package com.example.cardmatchinggame
+
+import com.example.cardmatchinggame.model.Box
+
+interface IBoxSelected {
+    fun onBoxSelected (box: Box)
+}
---
+++ app/src/main/java/com/example/cardmatchinggame/viewmodel/ItemImageViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.cardmatchinggame.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class ItemImageViewModel : ViewModel(){
+
+}
